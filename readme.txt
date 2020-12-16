f8c8b90e5b5af2513e9964327f55cd26bdf64843
Johannes Siipola <johannes.siipola@creuna.fi>
2020-12-16 13:24:41 +0200

    WordPress 5.6 support

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === Remove Drop Cap ===
 Requires at least: 5.5
-Tested up to: 5.5
+Tested up to: 5.6
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -8,6 +8,10 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Plugin to disable drop cap in Gutenberg editor paragraph block
 
 == Changelog ==
+
+= 1.1.0 (2020-12-16) =
+* Change: Use server side feature registration to disable dropCap instead of JavaScript
+* Fix: WordPress 5.6 support
 
 = 1.0.0 (2020-09-20) =
 * Initial release
--- remove-drop-cap.php
+++ remove-drop-cap.php
@@ -4,7 +4,7 @@
  * Plugin name: Disable Drop Cap
  * Description: Plugin to disable drop cap in Gutenberg editor paragraph block
  * Plugin URI: https://github.com/joppuyo/remove-drop-cap
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Johannes Siipola
  * Author URI: https://siipo.la
  * License: GPLv2 or later
@@ -19,35 +19,10 @@ $plugin_update_checker = Puc_v4_Factory::buildUpdateChecker(
     'remove-drop-cap'
 );
 
-add_action('admin_footer', function () {
-    echo <<<HTML
-<script>
-document.addEventListener("DOMContentLoaded", function () {
-  var removeDropCap = function(settings, name) {
-    if (name !== "core/paragraph") {
-      return settings;
+add_filter(
+    'block_editor_settings',
+    function ($editor_settings) {
+        $editor_settings['__experimentalFeatures']['global']['typography']['dropCap'] = false;
+        return $editor_settings;
     }
-
-    var newSettings = Object.assign({}, settings);
-
-    if (
-      newSettings.supports &&
-      newSettings.supports.__experimentalFeatures &&
-      newSettings.supports.__experimentalFeatures.typography &&
-      newSettings.supports.__experimentalFeatures.typography.dropCap
-    ) {
-      newSettings.supports.__experimentalFeatures.typography.dropCap = false;
-    }
-
-    return newSettings;
-  };
-
-  wp.hooks.addFilter(
-    "blocks.registerBlockType",
-    "sc/gb/remove-drop-cap",
-    removeDropCap,
-  );
-});
-</script>
-HTML;
-});
+);
