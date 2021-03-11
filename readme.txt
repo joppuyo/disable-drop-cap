fb1c683ddec793a109c6d8caed691ea42ae821a1
Johannes Siipola <johannes.siipola@knowit.fi>
2021-03-11 21:24:20 +0200

    Release v1.2.0

--- readme.txt
+++ readme.txt
@@ -9,6 +9,10 @@ Plugin to disable drop cap in Gutenberg editor paragraph block
 
 == Changelog ==
 
+= 1.2.0 (2021-03-11) =
+* Feature: WordPress 5.7 support
+* Fix: regression in WordPress 5.5
+
 = 1.1.0 (2020-12-16) =
 * Change: Use server side feature registration to disable dropCap instead of JavaScript
 * Fix: WordPress 5.6 support
--- remove-drop-cap.php
+++ remove-drop-cap.php
@@ -4,7 +4,7 @@
  * Plugin name: Disable Drop Cap
  * Description: Plugin to disable drop cap in Gutenberg editor paragraph block
  * Plugin URI: https://github.com/joppuyo/remove-drop-cap
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Johannes Siipola
  * Author URI: https://siipo.la
  * License: GPLv2 or later
@@ -22,7 +22,61 @@ $plugin_update_checker = Puc_v4_Factory::buildUpdateChecker(
 add_filter(
     'block_editor_settings',
     function ($editor_settings) {
-        $editor_settings['__experimentalFeatures']['global']['typography']['dropCap'] = false;
+        global $wp_version;
+
+        if (version_compare($wp_version, '5.7', '>=')) {
+            $editor_settings['__experimentalFeatures']['defaults']['typography']['dropCap'] = false;
+        }
+
+        if (
+            version_compare($wp_version, '5.6', '>=') &&
+            version_compare($wp_version, '5.7', '<')
+        ) {
+            $editor_settings['__experimentalFeatures']['global']['typography']['dropCap'] = false;
+        }
+
         return $editor_settings;
     }
 );
+
+
+add_action('admin_footer', function () {
+    global $wp_version;
+
+    if (
+        version_compare($wp_version, '5.5', '>=') &&
+        version_compare($wp_version, '5.6', '<'))
+    {
+
+        echo <<<HTML
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  var removeDropCap = function(settings, name) {
+      
+      console.log(settings)
+      
+    if (name !== "core/paragraph") {
+      return settings;
+    }
+    var newSettings = Object.assign({}, settings);
+    if (
+      newSettings.supports &&
+      newSettings.supports.__experimentalFeatures &&
+      newSettings.supports.__experimentalFeatures.typography &&
+      newSettings.supports.__experimentalFeatures.typography.dropCap
+    ) {
+      newSettings.supports.__experimentalFeatures.typography.dropCap = false;
+    }
+    return newSettings;
+  };
+  wp.hooks.addFilter(
+    "blocks.registerBlockType",
+    "sc/gb/remove-drop-cap",
+    removeDropCap,
+  );
+});
+</script>
+HTML;
+
+    }
+});
